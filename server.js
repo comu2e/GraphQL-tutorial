9fc6ad884cf08fed634a766bd745b73865af1466
英士 <comu2e@gmail.com>
2021-03-16 20:27:29 +0900

    初めてのGraphQL

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const {graphqlHTTP} = require('express-graphql');
+
+const {buildSchema} = require('graphql')
+//スーマはあくまで定義だけ
+//実際のデータ操作はしない
+
+const schema =buildSchema('type Query{hello:String}');
+
+//リゾルバ関数
+//特定のフィールドのデータを返す関数
+//実際のデータ操作を行う部分
+const root = {
+    hello:() => {
+        return 'Hello World!';
+    },
+};
+//expressでサーバを建てる
+const app = express();
+app.use('/graphql',graphqlHTTP({
+    schema:schema,
+    rootValue:root,
+    //GraphQLを利用できるようにtrueにする
+    graphiql:true
+}));
+
+app.listen(4000);
+console.log("Runnnig a graphQL api server at http://localhost:4000/graphql")
